2d1ac6633178cb3102a74365fd0510a20435b5fc
Joshua <1294686101@qq.com>
2019-11-22 23:51:22 +0800

    update

--- src/main/java/cn/royians/api/ServletAPI.java
+++ src/main/java/cn/royians/api/ServletAPI.java
@@ -3,14 +3,13 @@ package cn.royians.api;
 import cn.royians.utils.HttpClientUtil;
 import cn.royians.utils.XMLUtil;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 public class ServletAPI {
     @RequestMapping("/")
@@ -47,5 +46,24 @@ public class ServletAPI {
             json = JSON.parseObject("{'error':'500','info':'" + e.getMessage() + "'}");
         }
         return json;
+    }
+
+    @ResponseBody
+    @RequestMapping("/getTwitterHTML")
+    public String getTwitterHtml(@RequestParam(defaultValue = "AlexMtzyj", required = false) String name) {
+        StringBuilder html = new StringBuilder();
+        JSONObject twitter = this.getTwitter(name);
+        JSONArray jsonArray = twitter.getJSONObject("channel").getJSONArray("item");
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONObject item = jsonArray.getJSONObject(i);
+            html.append("<p><span>");
+            html.append(item.get("author"));
+            html.append("</span><small>");
+            html.append(item.get("pubDate"));
+            html.append("<small><br/><span>");
+            html.append(item.get("description"));
+            html.append("</span></p><hr/>");
+        }
+        return html.toString();
     }
 }
