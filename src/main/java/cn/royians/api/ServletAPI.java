133bc8881772b75e9835b8827c19747c22dad7b3
ROYIANS <z1294686101@gmail.com>
2019-11-29 16:12:55 +0800

    haha

--- src/main/java/cn/royians/api/ServletAPI.java
+++ src/main/java/cn/royians/api/ServletAPI.java
@@ -8,6 +8,8 @@ import com.alibaba.fastjson.JSONObject;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin
 @RestController
@@ -46,6 +48,21 @@ public class ServletAPI {
             json = JSON.parseObject("{'error':'500','info':'" + e.getMessage() + "'}");
         }
         return json;
+    }
+
+    @ResponseBody
+    @RequestMapping("/test")
+    public String test() {
+        RestTemplate restTemplate = new RestTemplate();
+		String url = "https://redirector.googlevideo.com/videoplayback?api=youtubemultidownloader.com&expire=1575016202&ei=qYLgXYSiN5On1wLNu6y4CA&ip=2a03%3Ab0c0%3A3%3Ae0%3A%3Ae6%3Af001&id=o-APB0LDLLB5FfWG-0fmU8hPPQU8ejM8plu2RmOKaWUwnC&itag=137&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C278%2C298%2C299%2C302%2C303&source=youtube&requiressl=yes&mm=31%2C26&mn=sn-4g5e6nsr%2Csn-f5f7lnel&ms=au%2Conr&mv=m&mvi=0&pl=48&initcwndbps=225000&mime=video%2Fmp4&gir=yes&clen=410313048&dur=1500.040&lmt=1574652681689759&mt=1574994512&fvip=1&keepalive=yes&fexp=23842630&txp=5535432&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=ALgxI2wwRQIhAMsVswqx-gR1K1_a9HNuzXfCezkF-hAubXPY1opdT-OQAiAxTxb1kkTzIGmdD_TzR5Xsp9vVSNx2liElqQb6OxtVfA%3D%3D&lsparams=mm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AHylml4wRAIgftbecU3_SLfYQAjfICzWkCV0S4-qxbdhGvLa9ntKLIACIFlsdN9sQbk3xusAqrWaXMeldl0CG-Ht3Sri7f_bBz-M&title=%E2%9C%96%EF%B8%8FEPIC+CHINA%3A+What+is+China%3F";
+		String testurl = "https://www.baidu.com";
+		String google = "https://www.google.com";
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(testurl, String.class);
+		HttpStatus statusCode = responseEntity.getStatusCode();
+		HttpHeaders headers = responseEntity.getHeaders();
+		System.out.println(statusCode.toString());
+		System.out.println(headers.toString());
+        return "over";
     }
 
     @ResponseBody
